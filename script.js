ea6d0042f878a9a46a2b4367a456694e041ac3f0
Ken Yamada <yamakenth@gmail.com>
2021-12-01 16:47:22 +0700

    Add function to return computer's move

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello world!');
+// randomly return 'rock', 'paper', 'scissors'
+function computerPlay() {
+  // move in english 
+  let move;
+  // generate a random number [0, 2]
+  let randNum = Math.floor(Math.random() * 3);
+  // generate a move 
+  if (randNum === 0) {
+    move = 'rock';
+  } else if (randNum === 1) {
+    move = 'paper';
+  } else {
+    move = 'scissors';
+  }
+  return move;
+}
+
