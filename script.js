4d71342e78aba3f342108e6e0d72fcd199c34d06
Ken Yamada <yamakenth@gmail.com>
2021-12-03 17:32:58 +0700

    Change logic to track game status by variables and add refresh button

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-// array to track round results and moves
-const gameResult = [];
+// variables to keep track of game status 
+let roundNumber = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 // eventListener on player selection buttons 
-// 1. play one round and add result to gameResult array
-// 2. display round result on page 
 const playerSelectionBtns = document.querySelectorAll('.selection-button');
 playerSelectionBtns.forEach((selectionBtn) => {
-  selectionBtn.addEventListener('click', (e) => {
-    // play one round 
-    let playerSelection = e.target.innerText.toLowerCase();
-    let computerSelection = generateComputerSelection();
-    let roundResult = playRound(playerSelection, computerSelection);
-    // add result and moves of each round to gameResult array 
-    gameResult.push([roundResult, playerSelection, computerSelection]); 
-    // get round result message
-    let roundResultMessage = getRoundResultMessage(gameResult[gameResult.length - 1]);
-    // add round result message to page 
-    addRoundResultToPage(roundResultMessage);
-    // add running total to page 
-    addRunningTotalToPage();
-  });
+  selectionBtn.addEventListener('click', (e) => playGame(e));
 });
+
+// play one round of game 
+// take in event from click 
+// return no results 
+function playGame(e) {
+  // add one round
+  roundNumber++;
+  // play one round 
+  let playerSelection = e.target.innerText.toLowerCase();
+  let computerSelection = generateComputerSelection();
+  let roundResult = playRound(playerSelection, computerSelection);
+  // get roundResult as message 
+  let roundResultMessage = getRoundResultMessage(roundResult, playerSelection, computerSelection);
+  // display roundResult 
+  displayRoundResultMessage(roundResultMessage);
+  // add scores 
+  addScore(roundResult);
+  // display running score 
+  displayRunningScore();
+  // end game if a score reaches 5 points 
+  if (playerScore === 5 || computerScore === 5) {
+    // display result message 
+    displayGameResultMessage();
+    // disable selection buttons 
+    disableSelectionBtns();
+    // display page refresh button
+    displayPlayAgain();
+  }
+}
 
 // randomly generate computer's move 
 // take no parameters 
@@ -47,7 +63,7 @@ function generateComputerSelection() {
 // Return int representation of result 
 function playRound(playerSelection, computerSelection) {
   // determine winner {-1: player lose, 0: draw, 1: player win}
-  let roundResult = NaN;
+  let roundResult = 0;
   if (playerSelection === computerSelection) { // draw 
     roundResult = 0;
   } else if (playerSelection === 'rock') { // player plays 'rock'
@@ -73,15 +89,23 @@ function playRound(playerSelection, computerSelection) {
   return roundResult;
 }
 
+// add score to playerScore or computerScore
+// take in roundResult int
+// return no result 
+function addScore(roundResult) {
+  if (roundResult === 1) {
+    playerScore++;
+  } else if (roundResult === -1) {
+    computerScore++;
+  }
+}
+
 // generate a message indicating who won in each round
-// take in array of roundResult, playerSelection, computerSelection
+// take in roundResult, playerSelection, computerSelection
 // return message
-function getRoundResultMessage(roundDetail) {
-  let roundResult = roundDetail[0];
-  let playerSelection = roundDetail[1];
-  let computerSelection = roundDetail[2];
+function getRoundResultMessage(roundResult, playerSelection, computerSelection) {
   if (roundResult === 0) {
-    return `It was a draw! You played ${playerSelection}.`;
+    return `It was a draw! You both played ${playerSelection}.`;
   } else if (roundResult === 1) {
     return `You won! ${playerSelection} beats ${computerSelection}`;
   } else {
@@ -89,34 +113,54 @@ function getRoundResultMessage(roundDetail) {
   }
 }
 
-// add child element to "Round Result" section on page
+// display roundResultMessage on page 
 // take in round result message 
 // return no results 
-function addRoundResultToPage(roundMessage) {
+function displayRoundResultMessage(roundMessage) {
   const roundResultDiv = document.querySelector('.round-result');
   const roundMessageP = document.createElement('p');
   roundMessageP.innerText = roundMessage;
   roundResultDiv.appendChild(roundMessageP);
 }
 
-// add running total to page and return message if one player wins 
+// display running total to page  
 // take in no parameters 
 // return no results 
-function addRunningTotalToPage() {
-  // calculate running totals 
-  let roundNumber = gameResult.length;
-  let resultOnly = gameResult.map(x => x[0]);
-  let playerScore = resultOnly.filter(x => x > 0).length;
-  let computerScore = resultOnly.filter(x => x < 0).length;
+function displayRunningScore() {
   // add running totals to page 
   document.querySelector('.round-number').innerText = roundNumber;
   document.querySelector('.player-score').innerText = playerScore;
   document.querySelector('.computer-score').innerText = computerScore;
-  // display game winner if one playe reaches 5 points 
+}
+
+// display game result on page
+// take in no parameters 
+// return no results 
+function displayGameResultMessage() {
   let gameResultDiv = document.querySelector('.game-result');
   let gameResultMessage = document.createElement('p');
-  if (playerScore === 5 || computerScore === 5) {
-    gameResultMessage.innerText = `You ${(playerScore > computerScore) ? 'won' : 'lost'}!`;
-    gameResultDiv.appendChild(gameResultMessage);
-  }
+  gameResultMessage.innerText = `You ${(playerScore > computerScore) ? 'won' : 'lost'}!`;
+  gameResultDiv.appendChild(gameResultMessage);
+}
+
+// disable player selection buttons 
+// take in no parameters
+// return no results 
+function disableSelectionBtns() {
+  playerSelectionBtns.forEach((selectionBtn) => {
+    selectionBtn.disabled = true;
+  });
+}
+
+// display play again to refresh page 
+// take in no parameters 
+// return no results 
+function displayPlayAgain() {
+  let playerSelectionDiv = document.querySelector('.player-selection');
+  let playAgainBtn = document.createElement('button');
+  playAgainBtn.type = 'button';
+  playAgainBtn.className = 'play-again';
+  playAgainBtn.innerText = 'Play again';
+  playerSelectionDiv.appendChild(playAgainBtn);
+  playAgainBtn.addEventListener('click', () => window.location.reload());
 }
